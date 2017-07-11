86fa07c61cdb1661f226f148118126b8dda0a3e4
orest <orest@HAL9000>
2017-07-11 23:14:26 +0200

    path requored to solve TypeError: path must be absolute or specify root to res.sendFile added

--- NodejsWebApp1/express.js
+++ NodejsWebApp1/express.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
-    res.send('<html><body><h1>Hello World</h1></body></html>');
+    res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
 });
 
-app.post('/submit-data', function (req, res) {
-    res.send('POST Request');
-});
+app.post('/submit-student-data', function (req, res) {
+    var name = req.body.firstName + ' ' + req.body.lastName;
 
-app.put('/update-data', function (req, res) {
-    res.send('PUT Request');
-});
-
-app.delete('/delete-data', function (req, res) {
-    res.send('DELETE Request');
+    res.send(name + ' Submitted Successfully!');
 });
 
 var server = app.listen(5000, function () {
     console.log('Node server is running..');
 });
+
